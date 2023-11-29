d5be2c1796bcfe65d527b829bd270e0340518a32
gianmarcodauria <150024279+gianmarcodauria@users.noreply.github.com>
2023-11-29 16:52:07 +0100

    Add files via upload

---
+++ ex05/git_commit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd user/Users/gd-auria/Desktop/PUSH2
+git log --format='%H' -n5
+
---
+++ ex06/git_ignore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Desktop/PUSH2
+git ls-files --others --exclude-from=.git/info/exclude
+
